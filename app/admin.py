14816802923a5960d20a57e56a105779336c5498
Johan Steven Arias V <johan@johan.(none)>
2014-09-03 22:58:40 -0500

    App running Perfectly. Static and Media Files Hostend in Amazon S3

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Actor
 
 class ActorAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('names', 'get_avatar')
 
 admin.site.register(Actor, ActorAdmin)
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 class Actor(models.Model):
 
@@ -7,6 +9,17 @@ class Actor(models.Model):
 	names = models.CharField(max_length=60)
 	picture = models.ImageField(upload_to='pictures')
 	genre = models.CharField(max_length=1, choices=genres)
+	avatar = ImageSpecField(source='picture', 
+				processors=[ResizeToFill(100, 100)], 
+				format='JPEG',
+				options={'quality': 100}
+				)
 
 	def __unicode__(self):
 		return self.names
+
+	def get_avatar(self):
+		return """<img src='%s' alt='%s'> """ %(self.avatar.url, self.names)
+
+	get_avatar.short_description = 'Picture'
+	get_avatar.allow_tags = True
